2dcd9613e63957ded6d2a9d287ae531c5d2de931
lazynormz <skupi1234@gmail.com>
2022-05-03 03:50:11 +0200

    Moved documentaion to the correct area

--- de4aber.cAppsule.Domain/IRepositories/IUserRepository.cs
+++ de4aber.cAppsule.Domain/IRepositories/IUserRepository.cs
@@ -7,6 +7,12 @@ namespace de4aber.cAppsule.Domain.IRepositories
     {
         public List<User> FindAll();
         
+        /// <summary>
+        /// Creates a new <c>User</c> entity and places it into the SQL-based database.
+        /// Before inserting into the database, it will confirm that the given username doesn't already exist within the database.
+        /// </summary>
+        /// <param name="user">the <c>User</c> object containing the user-data to be inserted</param>
+        /// <returns>The created <c>User</c> object, or throws an exception if already exists</returns>
         public User Create(User user);
     }
 }
