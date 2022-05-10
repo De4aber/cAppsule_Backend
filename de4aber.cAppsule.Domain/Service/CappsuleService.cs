fe46310e2abe04fd7c048acfb054bc2cf4aa80ef
lazynormz <skupi1234@gmail.com>
2022-05-10 16:12:14 +0200

    created basic service->repository functions

--- de4aber.cAppsule.Domain/Service/CappsuleService.cs
+++ de4aber.cAppsule.Domain/Service/CappsuleService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using de4aber.cAppsule.Core.IServices;
 using de4aber.cAppsule.Core.Models;
 using de4aber.cAppsule.Domain.IRepositories;
@@ -8,19 +10,24 @@ namespace de4aber.cAppsule.Domain.Service
     public class CappsuleService : ICappsuleService
     {
         private readonly ICappsuleRepository _repo;
+
+        public CappsuleService(ICappsuleRepository repo)
+        {
+            _repo = repo ?? throw new InvalidDataException("Repo cannot be null!");
+        }
         public Cappsule createCappsule(Cappsule cappsule)
         {
-            throw new System.NotImplementedException();
+            return _repo.createCappsule(cappsule);
         }
 
         public List<Cappsule> getCappsules(int userId)
         {
-            throw new System.NotImplementedException();
+            return _repo.getCappsules(userId);
         }
 
         public void deleteCappsules(int cappsuleId)
         {
-            throw new System.NotImplementedException();
+            _repo.deleteCappsule(cappsuleId);
         }
     }
 }
