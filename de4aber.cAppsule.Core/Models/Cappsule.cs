4d7ca59714f85637cebdd1c4822c7dc731150453
lazynormz <skupi1234@gmail.com>
2022-05-10 16:00:47 +0200

    Populated Cappsule.cs

--- de4aber.cAppsule.Core/Models/Cappsule.cs
+++ de4aber.cAppsule.Core/Models/Cappsule.cs
@@ -1,7 +1,15 @@
-﻿namespace de4aber.cAppsule.Core.Models
+﻿using de4aber.cAppsule.DataAccess.Entities;
+
+namespace de4aber.cAppsule.Core.Models
 {
     public class Cappsule
     {
-        
+        public int id { get; set; }
+        public int sender { get; set; }
+        public int reciever { get; set; }
+        public double lattitude { get; set; }
+        public double longitute { get; set; }
+        public string openTime { get; set; }
+        public Photo image { get; set; }
     }
 }
