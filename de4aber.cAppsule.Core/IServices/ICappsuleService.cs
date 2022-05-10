09f1c36971cbe851a0ca153a1870905f82cff19f
lazynormz <skupi1234@gmail.com>
2022-05-10 16:04:35 +0200

    Populated ICappsuleService.cs and imported empty functions in CappsuleService.cs

--- de4aber.cAppsule.Core/IServices/ICappsuleService.cs
+++ de4aber.cAppsule.Core/IServices/ICappsuleService.cs
@@ -1,7 +1,11 @@
-﻿namespace de4aber.cAppsule.Core.IServices
+﻿using de4aber.cAppsule.Core.Models;
+
+namespace de4aber.cAppsule.Core.IServices
 {
-    public class ICappsuleService
+    public interface ICappsuleService
     {
-        
+        public Cappsule createCappsule(Cappsule cappsule);
+        public Cappsule getCappsules(int userId);
+        public void deleteCappsules(int cappsuleId);
     }
 }
--- de4aber.cAppsule.Domain/Service/CappsuleService.cs
+++ de4aber.cAppsule.Domain/Service/CappsuleService.cs
@@ -1,9 +1,23 @@
 ﻿using de4aber.cAppsule.Core.IServices;
+using de4aber.cAppsule.Core.Models;
 
 namespace de4aber.cAppsule.Domain.Service
 {
     public class CappsuleService : ICappsuleService
     {
-        
+        public Cappsule createCappsule(Cappsule cappsule)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Cappsule getCappsules(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void deleteCappsules(int cappsuleId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
