ced16b2cbd28165e0831911d1be767c1509e7fdf
lazynormz <skupi1234@gmail.com>
2022-05-10 15:18:46 +0200

    Created basic cAppsuleDTO alongside a class for holding Photo-data

---
+++ de4aber.cAppsule.DataAccess/Entities/CappsuleEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace de4aber.cAppsule.DataAccess.Entities
+{
+    public class CappsuleEntity
+    {
+        public int id { get; set; }
+        public int sender { get; set; }
+        public int reciever { get; set; }
+        public double lattitude { get; set; }
+        public double longitute { get; set; }
+        public string openTime { get; set; }
+        public Photo image { get; set; }
+    }
+
+    public class Photo
+    {
+        public int Id { get; set; }
+        public byte[] Bytes { get; set; }
+        public string Description { get; set; }
+        public string FileExtension { get; set; }
+        public decimal Size { get; set; }
+    }
+}
