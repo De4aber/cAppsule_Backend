992bef1d20ffe7f41525e30fc837f31fcaf70d9d
lazynormz <skupi1234@gmail.com>
2022-05-10 15:56:48 +0200

    Created CappsuleDTO.cs

---
+++ de4aber.cAppsule.Core/DTOs/CappsuleDTO.cs
@@ -0,0 +1,14 @@
+﻿using de4aber.cAppsule.DataAccess.Entities;
+
+namespace de4aber.cAppsule.Core.DTOs
+{
+    public class CappsuleDTO
+    {
+        public int sender { get; set; }
+        public int reciever { get; set; }
+        public double lattitude { get; set; }
+        public double longitute { get; set; }
+        public string openTime { get; set; }
+        public Photo image { get; set; }
+    }
+}
