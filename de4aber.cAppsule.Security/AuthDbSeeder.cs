6720e129cc542bb58e4aa2301da615cfc98601b5
lazynormz <skupi1234@gmail.com>
2022-05-18 12:20:46 +0200

    DbSeeder

--- de4aber.cAppsule.Security/AuthDbSeeder.cs
+++ de4aber.cAppsule.Security/AuthDbSeeder.cs
@@ -1,6 +1,25 @@
+using de4aber.cAppsule.Security.IServices;
+using de4aber.cAppsule.Security.Services;
+
 namespace de4aber.cAppsule.Security;
 
 public class AuthDbSeeder : IAuthDbSeeder
 {
-    
+    private readonly AuthDbContext _ctx;
+    private readonly IAuthService _service;
+    public AuthDbSeeder(AuthDbContext ctx, IAuthService service)
+    {
+        _ctx = ctx;
+        _service = service;
+    }
+    public void SeedDevelopment()
+    {
+        _ctx.Database.EnsureDeleted();
+        _ctx.Database.EnsureCreated();
+    }
+
+    public void SeedProduction()
+    {
+        _ctx.Database.EnsureCreated();
+    }
 }
--- de4aber.cAppsule.Security/IAuthDbSeeder.cs
+++ de4aber.cAppsule.Security/IAuthDbSeeder.cs
@@ -2,5 +2,6 @@ namespace de4aber.cAppsule.Security;
 
 public interface IAuthDbSeeder
 {
-    
+    void SeedDevelopment();
+    void SeedProduction();
 }
