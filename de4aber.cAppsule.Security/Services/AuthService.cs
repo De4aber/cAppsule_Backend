f1e05e7a76d8ab5ecdfeeb60d216c1fdd620ad3e
lazynormz <skupi1234@gmail.com>
2022-05-18 11:50:49 +0200

    Linked AuthService to AuthRepository

--- de4aber.cAppsule.Security/Services/AuthService.cs
+++ de4aber.cAppsule.Security/Services/AuthService.cs
@@ -1,13 +1,20 @@
-﻿using de4aber.cAppsule.Security.IServices;
+﻿using de4aber.cAppsule.Security.IRepositories;
+using de4aber.cAppsule.Security.IServices;
 using de4aber.cAppsule.Security.Models;
+using de4aber.cAppsule.Security.Repositories;
 
 namespace de4aber.cAppsule.Security.Services
 {
     public class AuthService : IAuthService
     {
+        private readonly IAuthRepository _repo;
+        public AuthService(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
         public AuthUser GetUser(string username)
         {
-            throw new System.NotImplementedException();
+            return _repo.FindUser(username);
         }
     }
 }
