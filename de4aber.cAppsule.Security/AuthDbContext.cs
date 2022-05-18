81fdb1194bf8ee86fc7061301fde76c18745208a
lazynormz <skupi1234@gmail.com>
2022-05-18 12:00:00 +0200

    Populated method for finding AuthUserEnitity from Database

--- de4aber.cAppsule.Security/AuthDbContext.cs
+++ de4aber.cAppsule.Security/AuthDbContext.cs
@@ -1,6 +1,14 @@
+using de4aber.cAppsule.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace de4aber.cAppsule.Security;
 
-public class AuthDbContext
+public class AuthDbContext : DbContext
 {
-    
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+            
+    }
+
+    public DbSet<AuthUserEntity> AuthUsers { get; set; }
 }
--- de4aber.cAppsule.Security/Repositories/AuthRepository.cs
+++ de4aber.cAppsule.Security/Repositories/AuthRepository.cs
@@ -1,17 +1,31 @@
-﻿using de4aber.cAppsule.Security.IRepositories;
+﻿using de4aber.cAppsule.Security.Entities;
+using de4aber.cAppsule.Security.IRepositories;
 using de4aber.cAppsule.Security.Models;
 
 namespace de4aber.cAppsule.Security.Repositories
 {
     public class AuthRepository : IAuthRepository
     {
-        public AuthRepository()
+        private readonly AuthDbContext _ctx;
+        public AuthRepository(AuthDbContext ctx)
         {
-            
+            _ctx = ctx;
         }
+
+        private AuthUser EntityToUser(AuthUserEntity e)
+        {
+            return new AuthUser()
+            {
+                Id = e.Id,
+                Username = e.Username,
+                HashedPassword = e.HashedPassword
+            };
+        }
+        
         public AuthUser FindUser(string username)
         {
-            throw new System.NotImplementedException();
+            AuthUserEntity user = _ctx.AuthUsers.FirstOrDefault(user => username.Equals(user.Username));
+            return (user == null) ? null : EntityToUser(user);
         }
     }
 }
